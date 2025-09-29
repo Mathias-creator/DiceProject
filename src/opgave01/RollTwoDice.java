092fa02627a849ab3a4bead0a81ef1ab5eacaf93
mathiasskov <mathias.skov11@gmail.com>
2025-09-29 13:25:10 +0200

    aflevering 1

--- src/opgave01/RollTwoDice.java
+++ src/opgave01/RollTwoDice.java
@@ -1,4 +1,83 @@
 package opgave01;
 
+import java.util.Scanner;
+
 public class RollTwoDice {
+    private static int rollCount = 0;
+    private static int totalSum = 0;
+    private static int pairs = 0;
+    private static int highestNumber = 0;
+    private static int[] faceCounts = new int[6];
+
+    public static void main(String[] args) {
+        System.out.println("Velkommen til spillet, rul en terning.");
+        printRules();
+        System.out.println();
+
+        playTwoDice();
+
+        System.out.println();
+        System.out.println("Tak for at spille, rul en terning.");
+    }
+
+    private static void printRules() {
+        System.out.println("=====================================================");
+        System.out.println("Regler for rul en terning");
+        System.out.println("Spilleren ruller en terning, så længde man lyster.");
+        System.out.println("=====================================================");
+    }
+
+    private static int playTwoDice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Rul to terninger? ('ja/nej') ");
+        String answer = scanner.nextLine();
+        while (!answer.equals("nej")) {
+            int face1 = rollDie();
+            int face2 = rollDie();
+            int midSum = face1+face2;
+            System.out.println("Du rullede: " + face1 + " " + "og" + " " + face2);
+            System.out.println();
+            System.out.println("sum for dette rul " + midSum);
+            totalSum += midSum;
+            if (face1 == face2) {
+                pairs++;
+            }
+            if (highestNumber <= midSum){
+                highestNumber = midSum;
+            }
+            faceCounts[face1-1]++;
+            faceCounts[face2-1]++;
+
+            updateStatistics();
+
+            System.out.print("Rul en terning? ('ja/nej') ");
+            answer = scanner.nextLine();
+        }
+        printStatistics();
+        scanner.close();
+        return totalSum;
+    }
+
+    public static int rollDie() {
+        return (int) (Math.random() * 6 + 1);
+    }
+
+    private static void updateStatistics() {
+        rollCount++;
+    }
+
+    private static void printStatistics() {
+        System.out.println("\nResults:");
+        System.out.println("-------");
+        System.out.printf("%17s %4d\n", "Antal rul:", rollCount);
+        System.out.println("din samlede sum for alle rul: " + totalSum);
+        System.out.println("antal par rullet i alt: " + pairs);
+        System.out.println("højeste rullede antal øjne: " + highestNumber);
+        System.out.println("antal gange hver værdi forekommer:");
+        for (int i = 0; i < faceCounts.length; i++) {
+            System.out.println((i+1) + "'ere: " + faceCounts[i]);
+        }
+
+    }
+
 }
--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -1,4 +1,84 @@
 package opgave02;
-
+import java.util.Scanner;
 public class Craps {
+    private static int rollCount = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Velkommen til spillet, rul en terning.");
+        printRules();
+        System.out.println();
+
+        playCraps();
+
+        System.out.println();
+        System.out.println("Tak for at spille, craps.");
+    }
+
+    private static void printRules() {
+        System.out.println("=====================================================");
+        System.out.println("Regler for craps");
+        System.out.println("Første kast (come out roll):");
+        System.out.println("- 7 eller 11: Vinder med det samme");
+        System.out.println("- 2, 3 eller 12: Taber med det samme");
+        System.out.println("- 4, 5, 6, 8, 9, 10: Etablerer 'point'");
+        System.out.println("Efter point etableres:");
+        System.out.println("- Kaster point igen: Vinder");
+        System.out.println("- Kaster 7: Taber");
+        System.out.println("=====================================================");
+    }
+
+    private static void playCraps() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("spil craps (ja/nej)");
+        String answer = scanner.nextLine();
+        while (!answer.equals("nej")) {
+            int face = rollDie();
+            int face2 = rollDie();
+            int comeOutRoll = face + face2;
+            System.out.println("Du rullede: " + comeOutRoll);
+            System.out.println();
+            if (comeOutRoll == 7 || comeOutRoll == 11) {
+                System.out.println("Vinderkast! du har vundet");
+            } else if  (comeOutRoll == 2 || comeOutRoll == 3 || comeOutRoll == 12) {
+                System.out.println("taberkast! du har tabt");
+            } else {
+                int point = comeOutRoll;
+                System.out.println("point samlet" + point);
+                System.out.println("Nu skal du rulle for at score point...");
+                boolean won = rollForPoint(point);
+            }
+
+            System.out.print("Rul en terning? ('ja/nej') ");
+            answer = scanner.nextLine();
+        }
+        scanner.close();
+    }
+
+    public static boolean rollForPoint(int point) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("tryk enter for al rulle terningerne...");
+            scanner.nextLine();
+            int face = rollDie();
+            int face2 = rollDie();
+            int roll = face + face2;
+
+            System.out.println("Du rullede: " + face + " og " + face2 + " = " + roll );
+
+            if (roll == point) {
+                return true;
+            } else if (roll == 7){
+                return false;
+            } else {
+                System.out.println("Rullede " + roll + " Fortsætter...");
+            }
+        }
+    }
+    private static int rollDie() {
+        return (int) (Math.random() * 6 + 1);
+    }
+
+
 }
+
